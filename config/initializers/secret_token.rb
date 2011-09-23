a540a36957ad4788a0f7d6ae1f4e995a3f4f3f64
Amadeus <amadeusfolego@gmail.com>
2011-09-22 21:06:35 -0300

    Just rails new"d

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveYr::Application.config.secret_token = '1b30b30719f65d8f89428227d9dec6f330c2cb0586563584b566a06530625382dc2736cdfce4cc697a1159b05d388cfc0d51a122e63142931e1c0e2c3a00c308'
