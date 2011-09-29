224d7f75dc9f7575499514610655251b823abc3a
Amadeus <amadeusfolego@gmail.com>
2011-09-29 01:11:50 -0300

    Removind unnecessary relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,7 +3,6 @@ class Relationship < ActiveRecord::Base
 
   belongs_to :follower, :class_name => "User"
   belongs_to :followed, :class_name => "User"
-  belongs_to :followed_questionnaire, :class_name => "Questionnaire"
   validates_presence_of :follower_id, :followed_id
   validates_uniqueness_of :follower_id, :scope => :followed_id
 end
